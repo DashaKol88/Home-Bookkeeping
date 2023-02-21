f8934d104fd8670ee4a8521bef0dd8053fd4af2b
Dasha <dashyatamakarenko88@gmail.com>
2023-02-21 21:47:49 +0200

    create new form, create add_transaction

--- Home_book/hbm/urls.py
+++ Home_book/hbm/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='hbm/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='hbm/logout.html'), name='logout'),
     path('latest/', views.latest, name="latest"),
+    path('add_transaction/', views.add_transaction, name='add_transaction'),
 ]
--- Home_book/hbm/views.py
+++ Home_book/hbm/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
+from .forms import TransactionForm
 from .models import Transaction, Account
 
 
@@ -15,3 +16,29 @@ def latest(request):
     user_account = get_object_or_404(Account, account_owner=request.user)
     transactions = Transaction.objects.filter(transaction_account=user_account).order_by('-transaction_date')[:5]
     return render(request, 'hbm/transaction.html', {'transactions': transactions})
+
+
+@login_required
+def add_transaction(request):
+    if request.method == "POST":
+        form = TransactionForm(request.POST)
+        if form.is_valid():
+            transaction = form.save(commit=False)
+            user_account = get_object_or_404(Account, account_owner=request.user)
+            transaction.transaction_account = user_account
+            user_data = Account.objects.get(account_owner=request.user)
+            transaction.transaction_balance_now = user_data.account_balance
+            if transaction.transaction_type == 1:
+                transaction.transaction_balance_now += transaction.transaction_sum
+            else:
+                transaction.transaction_balance_now -= transaction.transaction_sum
+
+            user_data.account_balance = transaction.transaction_balance_now
+            user_data.save()
+            transaction.save()
+            return redirect('latest')
+    else:
+        form = TransactionForm()
+    return render(request, 'hbm/add_transaction.html', {"form": form})
+
+
